0297e2db6892b013fd64a92624dc190df04c2a9b
santipu <santipujolsau@gmail.com>
2022-10-26 17:17:45 +0200

    resolved bug with verify function

--- utils/verify.js
+++ utils/verify.js
@@ -1,17 +1,21 @@
 const { run } = require("hardhat")
 
 const verify = async (contractAddress, args) => {
-    console.log("Verifying Contract")
+    console.log("Verifying contract...")
     try {
-        await run("verfify:verify", {
+        await run("verify:verify", {
             address: contractAddress,
             constructorArguments: args,
         })
     } catch (e) {
         if (e.message.toLowerCase().includes("already verified")) {
-            console.log("Already Verified!")
+            console.log("Already verified!")
         } else {
             console.log(e)
         }
     }
+}
+
+module.exports = {
+    verify,
 }
