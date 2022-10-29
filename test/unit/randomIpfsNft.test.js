0846a6fcd2b4bd50566c473c57cc364a5e02b3a3
santipu <santipujolsau@gmail.com>
2022-10-29 21:23:39 +0200

    added describe section to test requestNFT function

--- test/unit/randomIpfsNft.test.js
+++ test/unit/randomIpfsNft.test.js
@@ -1,11 +1,11 @@
-const { assert } = require("chai")
+const { assert, expect } = require("chai")
 const { network, getNamedAccounts, deployments, ethers } = require("hardhat")
 const { developmentChains, networkConfig } = require("../../helper-hardhat-config")
 
 !developmentChains.includes(network.name)
     ? describe.skip
     : describe("RandomIPFSNFT Unit Tests", function () {
-          let randomIpfsNft, deployer, vrfCoordinatorV2Mock
+          let randomIpfsNft, deployer, vrfCoordinatorV2Mock, mintFee
           const chainId = network.config.chainId
 
           beforeEach(async () => {
@@ -13,6 +13,7 @@ const { developmentChains, networkConfig } = require("../../helper-hardhat-confi
               await deployments.fixture(["mocks", "randomipfs"])
               randomIpfsNft = await ethers.getContract("RandomIpfsNft", deployer)
               vrfCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock", deployer)
+              mintFee = await randomIpfsNft.getMintFee()
           })
 
           describe("constructor", () => {
@@ -23,6 +24,27 @@ const { developmentChains, networkConfig } = require("../../helper-hardhat-confi
                   assert.equal(name.toString(), "Random IPFS NFT")
                   assert.equal(symbol.toString(), "RIN")
                   assert.equal(mintFee.toString(), networkConfig[chainId]["mintFee"])
+              })
+          })
+
+          describe("request NFT", () => {
+              it("fails if payment isn't sent with the request", async () => {
+                  await expect(randomIpfsNft.requestNft()).to.be.revertedWith(
+                      "RandomIpfsNft__NeedMoreETHSent"
+                  )
+              })
+              it("reverts if payment amount is less than the mint fee", async () => {
+                  const fee = ethers.utils.parseEther("0.005")
+                  await expect(randomIpfsNft.requestNft({ value: fee })).to.be.revertedWith(
+                      "RandomIpfsNft__NeedMoreETHSent"
+                  )
+              })
+              it("emits an event and kicks off a random word request", async function () {
+                  const fee = await randomIpfsNft.getMintFee()
+                  await expect(randomIpfsNft.requestNft({ value: fee.toString() })).to.emit(
+                      randomIpfsNft,
+                      "NftRequested"
+                  )
               })
           })
       })
