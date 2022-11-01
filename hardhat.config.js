61b13633fe1c1909657878a9e8bbd6555fc6289e
santipu <santipujolsau@gmail.com>
2022-11-01 13:27:37 +0100

    added MockV3Aggregator to test contracts to deploy DynamicSvgNft in hardhat network

--- hardhat.config.js
+++ hardhat.config.js
@@ -15,7 +15,7 @@ const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
     solidity: {
-        compilers: [{ version: "0.8.8" }],
+        compilers: [{ version: "0.8.8" }, { version: "0.6.0" }],
     },
     defaultNetwork: "hardhat",
     networks: {
