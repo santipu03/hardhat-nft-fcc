4234c862ce142d153fff53be35ce7804d0ea3730
santipu <santipujolsau@gmail.com>
2022-11-01 13:34:27 +0100

    deployed MockV3Aggregator in deployMocks file

--- deploy/00-deploy-mocks.js
+++ deploy/00-deploy-mocks.js
@@ -1,5 +1,5 @@
 const { network } = require("hardhat")
-const { developmentChains } = require("../helper-hardhat-config")
+const { developmentChains, DECIMALS, INITIAL_PRICE } = require("../helper-hardhat-config")
 
 const BASE_FEE = ethers.utils.parseEther("0.25") // 0.25 is the premium. It costs 0.25 LINK
 const GAS_PRICE_LINK = 1e9
@@ -7,17 +7,21 @@ const GAS_PRICE_LINK = 1e9
 module.exports = async function ({ getNamedAccounts, deployments }) {
     const { deploy, log } = deployments
     const { deployer } = await getNamedAccounts()
-    const args = [BASE_FEE, GAS_PRICE_LINK]
 
     if (developmentChains.includes(network.name)) {
         log("Local network detected! Deploying mocks...")
         await deploy("VRFCoordinatorV2Mock", {
             from: deployer,
             log: true,
-            args: args,
+            args: [BASE_FEE, GAS_PRICE_LINK],
+        })
+        await deploy("MockV3Aggregator", {
+            from: deployer,
+            log: true,
+            args: [DECIMALS, INITIAL_PRICE],
         })
         log("Mocks deployed!")
     }
 }
 
-module.exports.tags = ["all", "mocks"]
+module.exports.tags = ["all", "mocks", "main"]
